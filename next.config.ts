f3fb0a795fce471a42919628f33bdfb6ca4b1a06
JBD-GER <info@justbedigital.de>
2026-07-02 08:51:37 +0200

    Add sofortvergleich redirects

--- next.config.ts
+++ next.config.ts
@@ -33,6 +33,22 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+  async redirects() {
+    return [
+      {
+        source: "/sofortvergleich/strom",
+        destination:
+          "https://a.check24.net/misc/click.php?pid=1164717&aid=18&deep=stromanbieter-wechseln&cat=1",
+        permanent: false,
+      },
+      {
+        source: "/sofortvergleich/internet",
+        destination:
+          "https://a.check24.net/misc/click.php?pid=1164717&aid=18&deep=dsl-anbieterwechsel&cat=4",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
